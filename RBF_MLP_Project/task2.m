b2ee762226ddac040faf8ec631b42f80182eab81
mpshos <mps9vf@gmail.com>
2019-11-24 22:10:58 -0700

    Add update code files. Remove unused files. Add figures

--- RBF_MLP_Project/task2.m
+++ RBF_MLP_Project/task2.m
@@ -14,29 +14,42 @@ T = (labels.T + 1) / 2;
 % samples_nrm = normc(samples);
 
 %% Set up networks
-net1 = newrbe(trainP,trainT,3.0);
+% net1 = newrbe(trainP,trainT,3.0);
+% 
+% y1v = sim(net1,valP);
+% mse1v=mse(y1v-valT);
+% 
+% [net, spread] = best_spread_rbe(trainP, trainT, valP, valT, [0.05, 0.1, 0.25, 0.33, 0.5, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]);
 
-y1v = sim(net1,valP);
-mse1v=mse(y1v-valT);
+spreads = 0.05 : 0.1 : 10;
 
-[net, spread] = best_spread_rbe(trainP, trainT, valP, valT, [0.05, 0.1, 0.25, 0.33, 0.5, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]);
-
-preds = sim(net, testP);
+[net, trainMse, valMse] = eval_spreads(trainP, trainT, valP, valT, spreads);
 
 figure(1)
-plotroc(testT, testP);
+plot(spreads, trainMse);
+title('Train MSE vs Spread');
 
-% Use threshold to determine class
-preds(preds >= 0.5) = 1;
-preds(preds < 0.5) = 0;
+figure(2)
+plot(spreads, valMse);
+title('Validation MSE vs Spread');
 
-% Calculate and plot ROC AUC
-[X, Y, T, AUC] = perfcurve(valT, preds, 1);
 
-disp(AUC);
+% preds = sim(net, testP);
 
-figure(2);
-confusionchart(valT, preds);
+% figure(1)
+% plotroc(testT, testP);
+% 
+% % Use threshold to determine class
+% preds(preds >= 0.5) = 1;
+% preds(preds < 0.5) = 0;
+% 
+% % Calculate and plot ROC AUC
+% [X, Y, T, AUC] = perfcurve(valT, preds, 1);
+% 
+% disp(AUC);
+% 
+% figure(2);
+% confusionchart(valT, preds);
 
 
 
